9674174496363e7a3b8180b32d743d122a7b823c
Lauren Agne <AgneLauren1@gmail.com>
2017-11-17 13:24:00 -0500

    index controller

---
+++ src/main/java/com/techelevator/controller/IndexController.java
@@ -0,0 +1,24 @@
+package com.techelevator.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.techelevator.model.Login;
+import com.techelevator.model.Registration;
+
+@Controller
+public class IndexController {
+	
+
+	@Autowired
+	
+	@RequestMapping(path="/", method=RequestMethod.GET)
+	public String getMainScreen(@ModelAttribute Registration registration, @ModelAttribute Login login) {
+		return "index";
+	}
+	
+}
