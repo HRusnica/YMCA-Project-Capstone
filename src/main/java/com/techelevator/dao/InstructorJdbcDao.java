1a29d6498a88fd3e3c8f6e8bc8413927b1c5e250
Jared Malvic <jarediscool92@gmail.com>
2017-11-20 11:02:53 -0500

    500 fix

--- src/main/java/com/techelevator/dao/InstructorJdbcDao.java
+++ src/main/java/com/techelevator/dao/InstructorJdbcDao.java
@@ -7,7 +7,7 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 
 @Component
-public class InstructorJdbcDao {
+public class InstructorJdbcDao implements InstructorDAO {
 	
 	private JdbcTemplate jdbcTemplate;
 	
