54b68314955724d23119cd412e4ebe86931b471b
Jeff Rhine <j.s.rhine@gmail.com>
2017-11-21 16:16:26 -0500

    delete

--- src/main/java/com/techelevator/dao/InstructorDAO.java
+++ src/main/java/com/techelevator/dao/InstructorDAO.java
@@ -2,18 +2,15 @@ package com.techelevator.dao;
 
 import java.util.List;
 
-<<<<<<< HEAD
+
 import com.techelevator.model.ScheduledClass;
 
-public interface InstructorDAO {
 
-	List<ScheduledClass> GetAllScheduledClassesByInstructor(int instructorId);
+
 	
-=======
-import com.techelevator.model.Instructor;
 
 public interface InstructorDAO {
+	 List<ScheduledClass> GetAllScheduledClassesByInstructor(int instructorId);
+	
+	}
 
-		
->>>>>>> bb66262f168e9981fe7c959aa0919478cf843eeb
-}
