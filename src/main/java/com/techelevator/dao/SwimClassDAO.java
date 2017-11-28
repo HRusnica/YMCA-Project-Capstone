9e2b64ddd764c0470831efa06a1e44ae6b80587b
Jared Malvic <jarediscool92@gmail.com>
2017-11-28 14:23:28 -0500

    Skills spreadsheet 1.2

--- src/main/java/com/techelevator/dao/SwimClassDAO.java
+++ src/main/java/com/techelevator/dao/SwimClassDAO.java
@@ -11,4 +11,5 @@ public interface SwimClassDAO {
 	public List<SwimClass> getAllClasses();
 	public List<Skill> getSkills(int classId);
 	public List<StudentSkill> getStudentSkills(int studentId);
+	public SwimClass getClassByClassId(int classId);
 }
