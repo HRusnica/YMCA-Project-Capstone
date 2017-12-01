be45bbba642bad18f426820f78a1dc7376c66544
Jeff Rhine <j.s.rhine@gmail.com>
2017-12-01 08:07:34 -0500

    db

--- database/data.sql
+++ database/data.sql
@@ -267,4 +267,57 @@ INSERT INTO skill_student (skill_id, student_id) VALUES (2,4);
 INSERT INTO skill_student (skill_id, student_id) VALUES (3,4);
 INSERT INTO skill_student (skill_id, student_id) VALUES (4,4);
 
+
+INSERT INTO skill_student (skill_id, student_id) VALUES (5,2);
+INSERT INTO skill_student (skill_id, student_id) VALUES (6,2);
+INSERT INTO skill_student (skill_id, student_id) VALUES (7,2);
+INSERT INTO skill_student (skill_id, student_id) VALUES (8,2);
+INSERT INTO skill_student (skill_id, student_id) VALUES (5,1);
+INSERT INTO skill_student (skill_id, student_id) VALUES (6,1);
+INSERT INTO skill_student (skill_id, student_id) VALUES (7,1);
+INSERT INTO skill_student (skill_id, student_id) VALUES (8,1);
+INSERT INTO skill_student (skill_id, student_id) VALUES (5,3);
+INSERT INTO skill_student (skill_id, student_id) VALUES (6,3);
+INSERT INTO skill_student (skill_id, student_id) VALUES (7,3);
+INSERT INTO skill_student (skill_id, student_id) VALUES (8,3);
+INSERT INTO skill_student (skill_id, student_id) VALUES (5,4);
+INSERT INTO skill_student (skill_id, student_id) VALUES (6,4);
+INSERT INTO skill_student (skill_id, student_id) VALUES (7,4);
+INSERT INTO skill_student (skill_id, student_id) VALUES (8,4);
+
+INSERT INTO skill_student (skill_id, student_id) VALUES (1,5);
+INSERT INTO skill_student (skill_id, student_id) VALUES (2,5);
+INSERT INTO skill_student (skill_id, student_id) VALUES (3,5);
+INSERT INTO skill_student (skill_id, student_id) VALUES (4,5);
+INSERT INTO skill_student (skill_id, student_id) VALUES (1,6);
+INSERT INTO skill_student (skill_id, student_id) VALUES (2,6);
+INSERT INTO skill_student (skill_id, student_id) VALUES (3,6);
+INSERT INTO skill_student (skill_id, student_id) VALUES (4,6);
+INSERT INTO skill_student (skill_id, student_id) VALUES (1,7);
+INSERT INTO skill_student (skill_id, student_id) VALUES (2,7);
+INSERT INTO skill_student (skill_id, student_id) VALUES (3,7);
+INSERT INTO skill_student (skill_id, student_id) VALUES (4,7);
+INSERT INTO skill_student (skill_id, student_id) VALUES (1,8);
+INSERT INTO skill_student (skill_id, student_id) VALUES (2,8);
+INSERT INTO skill_student (skill_id, student_id) VALUES (3,8);
+INSERT INTO skill_student (skill_id, student_id) VALUES (4,8);
+
+
+INSERT INTO skill_student (skill_id, student_id) VALUES (5,5);
+INSERT INTO skill_student (skill_id, student_id) VALUES (6,5);
+INSERT INTO skill_student (skill_id, student_id) VALUES (7,5);
+INSERT INTO skill_student (skill_id, student_id) VALUES (8,5);
+INSERT INTO skill_student (skill_id, student_id) VALUES (5,6);
+INSERT INTO skill_student (skill_id, student_id) VALUES (6,6);
+INSERT INTO skill_student (skill_id, student_id) VALUES (7,6);
+INSERT INTO skill_student (skill_id, student_id) VALUES (8,6);
+INSERT INTO skill_student (skill_id, student_id) VALUES (5,7);
+INSERT INTO skill_student (skill_id, student_id) VALUES (6,7);
+INSERT INTO skill_student (skill_id, student_id) VALUES (7,7);
+INSERT INTO skill_student (skill_id, student_id) VALUES (8,7);
+INSERT INTO skill_student (skill_id, student_id) VALUES (5,8);
+INSERT INTO skill_student (skill_id, student_id) VALUES (6,8);
+INSERT INTO skill_student (skill_id, student_id) VALUES (7,8);
+INSERT INTO skill_student (skill_id, student_id) VALUES (8,8);
+
 COMMIT;
